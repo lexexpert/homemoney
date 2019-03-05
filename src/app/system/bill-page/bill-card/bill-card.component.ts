7811f668f1e54042a091b3e06ee3c2aca8702ebb
Ivan <ivanreshetar@gmail.com>
2019-03-04 22:46:26 -0600

    bill-card component updated

--- src/app/system/bill-page/bill-card/bill-card.component.ts
+++ src/app/system/bill-page/bill-card/bill-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {BillModel} from '../../shared/models/bill.model';
 
 @Component({
   selector: 'app-bill-card',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bill-card.component.scss']
 })
 export class BillCardComponent implements OnInit {
+
+  @Input() bill: BillModel;
+  @Input() currency: any;
+
+  dollar: number;
+  gryvna: number;
 
   constructor() { }
 
--- src/app/system/bill-page/bill-page.component.ts
+++ src/app/system/bill-page/bill-page.component.ts
@@ -9,23 +9,42 @@ import {BillModel} from '../shared/models/bill.model';
   styleUrls: ['./bill-page.component.scss']
 })
 export class BillPageComponent implements OnInit, OnDestroy {
-  subscription: Subscription;
+  sub1: Subscription;
+  sub2: Subscription;
+
+  currency: any;
+  bill: BillModel;
+
+  isLoaded = false;
 
   constructor(
     private billService: BillService
   ) { }
 
   ngOnInit() {
-    this.subscription = combineLatest(
+    this.sub1 = combineLatest(
       this.billService.getBill(),
       this.billService.getCurrency()
     ).subscribe((data: [BillModel, any]) => {
       console.log(data);
+      this.bill = data[0];
+      this.currency = data[1];
+      this.isLoaded = true;
     });
   }
 
+  onRefresh() {
+    this.isLoaded = false;
+    this.sub2 = this.billService.getCurrency()
+      .subscribe((currency: any) => {
+        this.currency = currency;
+        this.isLoaded = true;
+      });
+  }
+
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    this.sub1.unsubscribe();
+    this.sub2.unsubscribe();
   }
 
 }
