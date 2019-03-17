b947424309d28e7bb88e23d530ae45ed16d8f156
Ivan <ivanreshetar@gmail.com>
2019-03-16 19:07:32 -0500

    animated loader created

---
+++ src/app/shared/components/loader/loader.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-loader',
+  template: '<div class="loader-animator"></div>',
+  styleUrls: ['./loader.component.scss']
+})
+
+export class LoaderComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,20 @@
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgxChartsModule} from '@swimlane/ngx-charts';
+import {LoaderComponent} from './components/loader/loader.component';
 
 @NgModule({
-  imports: [ReactiveFormsModule, FormsModule, NgxChartsModule],
-  exports: [ReactiveFormsModule, FormsModule, NgxChartsModule]
+  declarations: [LoaderComponent],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    NgxChartsModule
+  ],
+  exports: [
+    ReactiveFormsModule,
+    FormsModule,
+    NgxChartsModule,
+    LoaderComponent
+  ]
 })
 export class SharedModule {}
